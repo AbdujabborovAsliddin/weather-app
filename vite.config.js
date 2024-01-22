376218c43dccc489d3c4b3058a51815b6767dc45
Asliddin <2010001.asliddin.abdujabborov@gmail.com>
2024-01-22 16:06:23 +0500

    first commit

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+});
+git remote add origin https://github.com/AbdujabborovAsliddin/weather_app.git
